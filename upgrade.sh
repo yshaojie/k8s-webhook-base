121271c98939d6e0f1d26c7eb5297cd4fc0ab73f
岳少杰 <shaojie.yue@beyonca.com>
2023-03-15 15:54:53 +0800

    init

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,9 @@
 tag=$(date '+%m%d%H%M%S')
 image_name=base-webhook
 #清除以前的镜像
-crictl images | grep docker.io/library/${image_name} | awk '{print $3}' |  xargs crictl rmi
+#docker exec -it kind-control-plane bash -c "crictl images | grep docker.io/library/${image_name} | awk '{print \$1\":\"\$2}' |  xargs crictl rmi"
+#清除以前的镜像，否则通过kind load docker-image加载的镜像无效
+docker images | grep ${image_name} | awk '{print $1":"$2}' |  xargs docker rmi
 make docker-build IMG=${image_name}:${tag}
 kind load docker-image ${image_name}:${tag}
 sleep 3
