40073071fb66f0ab6bb429debb73592bc98df54a
岳少杰 <shaojie.yue@beyonca.com>
2023-03-15 15:17:12 +0800

    init

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,8 @@
 tag=$(date '+%m%d%H%M%S')
-make docker-build IMG=base-webhook:${tag}
-kind load docker-image base-webhook:${tag}
+image_name=base-webhook
+#清除以前的镜像
+crictl images | grep docker.io/library/${image_name} | awk '{print $3}' |  xargs crictl rmi
+make docker-build IMG=${image_name}:${tag}
+kind load docker-image ${image_name}:${tag}
 sleep 3
-make deploy IMG=base-webhook:${tag}
+make deploy IMG=${image_name}:${tag}
